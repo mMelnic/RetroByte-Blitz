944a8f6cbe48fed0225fcf1cc49759d2c84a9a13
Mihaela Melnic <moldovamelnik@gmail.com>
2024-11-06 15:20:22 +0100

    Created android project

---
+++ app/src/main/java/hu/bme/aut/fvf13l/retrobyteblitz/MainActivity.kt
@@ -0,0 +1,47 @@
+package hu.bme.aut.fvf13l.retrobyteblitz
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import hu.bme.aut.fvf13l.retrobyteblitz.ui.theme.RetroByteBlitzTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RetroByteBlitzTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RetroByteBlitzTheme {
+        Greeting("Android")
+    }
+}
