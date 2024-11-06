f5405d0f97629afdac98a2abc0313bd1a61f01f6
Mihaela Melnic <moldovamelnik@gmail.com>
2024-11-06 22:47:59 +0100

    Entities and database set up

---
+++ app/src/main/java/hu/bme/aut/fvf13l/retrobyteblitz/auth/User.kt
@@ -0,0 +1,10 @@
+package hu.bme.aut.fvf13l.retrobyteblitz.auth
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey val username: String,
+    val hashedPassword: String
+)
