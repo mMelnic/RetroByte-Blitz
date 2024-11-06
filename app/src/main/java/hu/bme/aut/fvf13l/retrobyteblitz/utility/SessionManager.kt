54470b5f8d619a2d4655255c527ab6b66a0df259
Mihaela Melnic <moldovamelnik@gmail.com>
2024-11-06 23:02:57 +0100

    Storing user session in shared preferences

---
+++ app/src/main/java/hu/bme/aut/fvf13l/retrobyteblitz/utility/SessionManager.kt
@@ -0,0 +1,67 @@
+package hu.bme.aut.fvf13l.retrobyteblitz.utility
+
+import android.content.Context
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+
+object SessionManager {
+
+    private const val PREF_NAME = "user_session"
+    private const val USERNAME_KEY = "username"
+
+    fun saveUserSession(context: Context, username: String) {
+        val masterKey = MasterKey.Builder(context)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        val sharedPreferences = EncryptedSharedPreferences.create(
+            context,
+            PREF_NAME,
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
+        with(sharedPreferences.edit()) {
+            putString(USERNAME_KEY, username)
+            apply()
+        }
+    }
+
+    // Check if user is logged in by checking if username exists
+    fun isLoggedIn(context: Context): Boolean {
+        val masterKey = MasterKey.Builder(context)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        val sharedPreferences = EncryptedSharedPreferences.create(
+            context,
+            PREF_NAME,
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
+        return sharedPreferences.contains(USERNAME_KEY)
+    }
+
+    // Clear session at logout (implement logout feature later in the settings view)
+    fun clearSession(context: Context) {
+        val masterKey = MasterKey.Builder(context)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        val sharedPreferences = EncryptedSharedPreferences.create(
+            context,
+            PREF_NAME,
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
+        with(sharedPreferences.edit()) {
+            clear()
+            apply()
+        }
+    }
+}
