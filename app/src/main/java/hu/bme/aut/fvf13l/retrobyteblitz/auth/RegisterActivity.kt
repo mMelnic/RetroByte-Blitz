e4e1af01cf845198baccffcf9ffda51879c1da03
Mihaela Melnic <moldovamelnik@gmail.com>
2024-11-07 23:35:25 +0100

    AuthViewModel instantiation fix with factory

--- app/src/main/java/hu/bme/aut/fvf13l/retrobyteblitz/auth/RegisterActivity.kt
+++ app/src/main/java/hu/bme/aut/fvf13l/retrobyteblitz/auth/RegisterActivity.kt
@@ -8,10 +8,16 @@ import androidx.appcompat.app.AppCompatActivity
 import hu.bme.aut.fvf13l.retrobyteblitz.MainActivity
 import hu.bme.aut.fvf13l.retrobyteblitz.databinding.ActivityRegisterBinding
 import hu.bme.aut.fvf13l.retrobyteblitz.viewmodel.AuthViewModel
+import hu.bme.aut.fvf13l.retrobyteblitz.viewmodel.AuthViewModelFactory
 
 class RegisterActivity : AppCompatActivity() {
 
-    private val authViewModel: AuthViewModel by viewModels()
+    private val userDao by lazy { UserDatabase.getDatabase(this).userDao() }
+
+    // Use AuthViewModelFactory to inject UserDao into AuthViewModel
+    private val authViewModel: AuthViewModel by viewModels {
+        AuthViewModelFactory(userDao)
+    }
     private lateinit var binding: ActivityRegisterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +32,7 @@ class RegisterActivity : AppCompatActivity() {
             if (username.isNotBlank() && password.isNotBlank()) {
                 authViewModel.register(username, password) { success ->
                     if (success) {
-                        startActivity(Intent(this, MainActivity::class.java))
+                        startActivity(Intent(this, LoginActivity::class.java))
                         finish()
                     } else {
                         Toast.makeText(this, "Username already exists", Toast.LENGTH_SHORT).show()
