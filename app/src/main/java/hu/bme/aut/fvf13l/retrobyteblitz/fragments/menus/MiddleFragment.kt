5f72807a12d16120d70d5a8ca116e7dccced24dd
Mihaela Melnic <moldovamelnik@gmail.com>
2024-11-08 23:55:52 +0100

    Initial implementation of menu fragments

---
+++ app/src/main/java/hu/bme/aut/fvf13l/retrobyteblitz/fragments/menus/MiddleFragment.kt
@@ -0,0 +1,48 @@
+package hu.bme.aut.fvf13l.retrobyteblitz.fragments.menus
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import hu.bme.aut.fvf13l.retrobyteblitz.MainActivity
+import hu.bme.aut.fvf13l.retrobyteblitz.databinding.FragmentMiddleBinding
+
+class MiddleFragment : Fragment() {
+
+    private var _binding: FragmentMiddleBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMiddleBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonDailyActivities.setOnClickListener {
+            (activity as MainActivity).openCategory("Daily Activities")
+        }
+        binding.buttonLogic.setOnClickListener {
+            (activity as MainActivity).openCategory("Logic")
+        }
+        binding.buttonMemory.setOnClickListener {
+            (activity as MainActivity).openCategory("Memory")
+        }
+        binding.buttonCalculation.setOnClickListener {
+            (activity as MainActivity).openCategory("Calculation")
+        }
+        binding.buttonKnowledge.setOnClickListener {
+            (activity as MainActivity).openCategory("Knowledge")
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
