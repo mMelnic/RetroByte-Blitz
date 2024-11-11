5da691b775662a1369ebcc51f3ba2da7f6d6c0d2
Mihaela Melnic <moldovamelnik@gmail.com>
2024-11-11 23:37:05 +0100

    Recycler View dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation(libs.firebase.database)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.gridlayout)
+    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
